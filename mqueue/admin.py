0e5fcb53346c06bb282cbc57f7d9eb54d06f7820
syn <synwe@yahoo.fr>
2016-03-09 05:37:49 +0000

    Events manager + admin_url field

--- mqueue/admin.py
+++ mqueue/admin.py
@@ -8,12 +8,17 @@ from mqueue.models import MEvent
 def link_to_object(obj):
     return '<a href="'+obj.url+'" target="_blank">'+obj.url+'</a>'
 
+def link_to_object_admin(obj):
+    return '<a href="'+obj.admin_url+'" target="_blank">'+obj.url+'</a>'
+
 
 @admin.register(MEvent)
 class MEventAdmin(admin.ModelAdmin):
     read_only = ['date_posted', 'content_type']
-    list_display = ['name', link_to_object, 'content_type', 'date_posted']
+    list_display = ['name', link_to_object, link_to_object_admin, 'content_type', 'date_posted']
     link_to_object.allow_tags = True   
-    link_to_object.short_description = _(u'See')
+    link_to_object.short_description = _(u'See on site')
+    link_to_object_admin.allow_tags = True   
+    link_to_object_admin.short_description = _(u'See in admin')
 
 
--- mqueue/models.py
+++ mqueue/models.py
@@ -5,13 +5,43 @@ from django.utils.translation import ugettext_lazy as _
 from django.contrib.contenttypes.models import ContentType
 
 
+class MEventManager(models.Manager):
+    def create(self, *args, **kwargs):
+        if not 'name' in kwargs.keys():
+            raise ValueError(u"You must provide a 'name' argument for the MEvent")
+        else:
+            name = kwargs['name']
+        if not 'model' in kwargs.keys():
+            raise ValueError(u"You must provide a 'model' argument for the MEvent")
+        else:
+            content_type = ContentType.objects.get_for_model(kwargs['model'])
+        if not 'obj_pk' in kwargs.keys():
+            raise ValueError(u"You must provide an 'obj_pk' argument for the MEvent")
+        else:
+            obj_pk = kwargs['obj_pk'] 
+        notes = ''
+        if 'notes' in kwargs.keys():
+            notes = kwargs['notes']
+        url = ''
+        if 'url' in kwargs.keys():
+            url = kwargs['url']
+        admin_url = ''
+        if 'admin_url' in kwargs.keys():
+            admin_url = kwargs['admin_url']
+        mevent = MEvent(name=name, content_type=content_type, obj_pk=obj_pk, url=url, admin_url=admin_url, notes=notes)
+        mevent.save()
+        return mevent
+
+
 class MEvent(models.Model):
     content_type = models.ForeignKey(ContentType, verbose_name=_(u"Content type"))
     name = models.CharField(max_length=120, verbose_name=_(u"Name"))
-    url = models.CharField(max_length=255, verbose_name=_(u"Url"))
+    url = models.CharField(max_length=255, blank=True, verbose_name=_(u"Url"))
+    admin_url = models.CharField(max_length=255, blank=True, verbose_name=_(u"Admin url"))
     date_posted = models.DateTimeField(auto_now_add=True, verbose_name=_(u"Date posted"))
     obj_pk = models.IntegerField(verbose_name=_(u"Object primary key"))
     notes = models.TextField(blank=True)
+    events = MEventManager()
     
     class Meta:
         verbose_name = _(u'Events')
@@ -20,3 +50,8 @@ class MEvent(models.Model):
         
     def __unicode__(self):
         return self.name
+    
+    
+        
+        
+        
