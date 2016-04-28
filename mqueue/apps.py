cc0d284afa04bd8fe34787a9c9dba739e2c3bebe
syn <synwe@yahoo.fr>
2016-04-28 19:17:42 +0000

    Removed some black magic

--- mqueue/apps.py
+++ mqueue/apps.py
@@ -1,18 +1,6 @@
 from django.utils.translation import ugettext_lazy as _
 from django.apps import AppConfig
-import inspect
 
-
-def tracking_load_registry(*args, **kwargs):
-    """
-    Thanks to https://github.com/emencia/emencia-django-tracking
-    """
-    stack = inspect.stack()
-
-    for stack_info in stack[1:]:
-        if 'tracking_load_registry' in stack_info[3]:
-            return
-        
 
 class MqueueConfig(AppConfig):
     name = "mqueue"
@@ -20,6 +8,5 @@ class MqueueConfig(AppConfig):
     
     def ready(self):
         from mqueue import signals
-        tracking_load_registry()
         
         
