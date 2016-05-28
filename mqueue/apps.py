60f5135daf449e7fb171980b6de7fbdc8879488c
syn <synwe@yahoo.fr>
2016-05-28 14:17:11 +0000

    Models autoregistration

--- mqueue/apps.py
+++ mqueue/apps.py
@@ -1,3 +1,4 @@
+import importlib
 from django.utils.translation import ugettext_lazy as _
 from django.apps import AppConfig
 
@@ -8,5 +9,20 @@ class MqueueConfig(AppConfig):
     
     def ready(self):
         from mqueue import signals
+        # models registration from settings
+        from django.conf import settings
+        from mqueue.tracking import mqueue_tracker
+        AUTOREGISTER = getattr(settings, 'MQUEUE_AUTOREGISTER', [])
+        for modtup in AUTOREGISTER:
+            modpath = modtup[0]
+            level = modtup[1]
+            modsplit = modpath.split('.')
+            path = '.'.join(modsplit[:-1])
+            modname = '.'.join(modsplit[-1:])
+            module = importlib.import_module(path)
+            model = getattr(module, modname)
+            mqueue_tracker.register(model, level)
+            
+        
         
         
