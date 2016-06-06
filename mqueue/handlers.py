f256963d108ab6ea82d64eaaa462141d4232f8ca
syn <synwe@yahoo.fr>
2016-06-06 13:55:15 +0000

    Bugfix in log handler

--- mqueue/handlers.py
+++ mqueue/handlers.py
@@ -1,9 +1,8 @@
 # -*- coding: utf-8 -*-
 
-import traceback
 from logging import Handler
 from django.conf import settings
-from django.contrib.auth import get_user
+import traceback
 
  
 class LogsDBHandler(Handler,object):
@@ -13,23 +12,19 @@ class LogsDBHandler(Handler,object):
         msg = record.getMessage()
         name= msg
         if record.exc_info:
-            ex_type = str(record.exc_info[0])
-            ex_title =  str(record.exc_info[1])
+            ex_type = repr((record.exc_info[0]))
+            ex_title =  repr(record.exc_info[1])
             ex_traceback = '\n'.join(traceback.format_tb(record.exc_info[2]))
             msg+='\n\n'+ex_title+'\n\n'+ex_type+'\n\n'+ex_traceback
         if settings.DEBUG is True:
             event_class = 'Dev log '+record.levelname
         else:
             event_class = 'Log '+record.levelname
-        user=get_user(record.request)
-        from django.contrib.auth.models import AnonymousUser
-        if isinstance(user, AnonymousUser):
-            user = None
         MEvent.objects.create(
                               name=name, 
                               event_class=event_class, 
                               notes=msg, 
-                              user=user, 
+                              user=record.request.user, 
                               request=record.request,
                               url=record.request.path,
                               )
