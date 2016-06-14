17d0a9b9349782eb676fa74cde10ed2814437c6d
syn <synwe@yahoo.fr>
2016-06-14 11:53:15 +0000

    Version

--- setup.py
+++ setup.py
@@ -21,8 +21,5 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 2.7',
     ],
-  install_requires=[
-        "Django",
-    ],
   zip_safe=False
 )
