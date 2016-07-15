b6006a91cde4440c20cc5c506705b62248cda95c
syn <synwe@yahoo.fr>
2016-07-15 13:21:53 +0000

    Interface to broadcast messages to channels

--- mqueue/urls.py
+++ mqueue/urls.py
@@ -1,9 +1,13 @@
 # -*- coding: utf-8 -*-
 
+from django.core.urlresolvers import reverse_lazy
 from django.conf.urls import patterns, url
-from mqueue.views import MQueueView
+from django.views.generic import RedirectView
+from mqueue.views import BroadcastView
 
 
 urlpatterns = patterns('',
-    url(r'^', MQueueView.as_view(), name="mqueue-view"),
+    url(r'^redirect-home/$', RedirectView.as_view(url=reverse_lazy('mqueue-broadcast')), name='mqueue-message-broadcasted'),
+    #url(r'^ok/$', MessageBroadcastedView.as_view(pattern_name='mqueue-message-broadcasted'), name="mqueue-message-broadcasted"),
+    url(r'^', BroadcastView.as_view(), name="mqueue-broadcast"),
 )
--- mqueue/views.py
+++ mqueue/views.py
@@ -1,16 +1,27 @@
 # -*- coding: utf-8 -*-
-"""
-from django.views.generic import ListView
+
+from django.core.urlresolvers import reverse
+from django.views.generic import FormView
+from django.views.generic.base import RedirectView
+from django.utils.translation import ugettext_lazy as _
+from django.contrib import messages
 from mqueue.models import MEvent
+from mqueue.forms import BroadcastForm
 
 
-class MQueueView(ListView):
-    template_name = 'mqueue/view_queue.html'
-    paginate_by = 50
-    context_object_name = 'events'
+class BroadcastView(FormView):
+    form_class = BroadcastForm
+    template_name = 'mqueue/broadcast.html'
     
-    def get_queryset(self):
-        qs = MEvent.objects.all()
-        print "user="+str(self.request.user)
-        return qs
-"""
+    def form_valid(self, form):
+        msg = form.cleaned_data['message']
+        event_class = form.cleaned_data['event_class']
+        channel = form.cleaned_data['channel']
+        MEvent.objects.create(name=msg, event_class=event_class, commit=False, stream=True, channel=channel)
+        messages.info(self.request, _(u"Message broadcasted to channel "+channel))
+        return super(BroadcastView, self).form_valid(form)
+    
+    def get_success_url(self):
+        return reverse('mqueue-message-broadcasted')
+
+
