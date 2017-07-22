4f69534991d68eb1d0a7fa8370c084b64b147f2a
Win <synwe@yahoo.com>
2017-07-22 15:34:49 +0000

    Add requirements for hooks

--- setup.py
+++ setup.py
@@ -4,25 +4,27 @@ from setuptools import setup, find_packages
 version = __import__('mqueue').__version__
 
 setup(
-  name = 'django-mqueue',
-  packages=find_packages(),
-  include_package_data=True,
-  version = version,
-  description = 'Events queue for Django',
-  author = 'synw',
-  author_email = 'synwe@yahoo.com',
-  url = 'https://github.com/synw/django-mqueue', 
-  download_url = 'https://github.com/synw/django-mqueue/releases/tag/'+version, 
-  keywords = ['django', 'logging', 'monitoring'], 
-  classifiers = [
+    name='django-mqueue',
+    packages=find_packages(),
+    include_package_data=True,
+    version=version,
+    description='Events queue for Django',
+    author='synw',
+    author_email='synwe@yahoo.com',
+    url='https://github.com/synw/django-mqueue',
+    download_url='https://github.com/synw/django-mqueue/releases/tag/' + version,
+    keywords=['django', 'logging', 'monitoring'],
+    classifiers=[
         'Development Status :: 4 - Beta',
         'Framework :: Django :: 1.11',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3.5',
     ],
-  install_requires=[
+    install_requires=[
         'jsonfield',
+        'redis',
+        'influxdb'
     ],
-  zip_safe=False
+    zip_safe=False
 )
