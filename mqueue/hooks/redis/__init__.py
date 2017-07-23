f0ef4f5e269d7f2d7fd347e8f458c1c9ce1ffb34
Win <synwe@yahoo.com>
2017-07-23 11:50:16 +0000

    Fix bug in redis hook

--- mqueue/hooks/redis/__init__.py
+++ mqueue/hooks/redis/__init__.py
@@ -6,10 +6,14 @@ from mqueue.conf import HOOKS
 
 conf = HOOKS["redis"]
 R = redis.StrictRedis(host=conf["host"], port=conf["port"], db=conf["db"])
-event_num = int(time.time()) 
+event_num = int(time.time())
+
 
 def save(event, conf):
-    name = DOMAIN+"_event"+str(event_num)
+    global event_num
+    global R
+    name = DOMAIN + "_event" + str(event_num)
     event.request = event.request.replace("\n", "//")
-    data = serializer.Pack(event)  
+    data = serializer.Pack(event)
     R.set(name, data)
+    event_num += 1
