76d45475090144903ec3421491dc5f998f67e236
Win <synwe@yahoo.com>
2017-07-25 09:32:04 +0000

    Add error handling on module import at initilization time

--- mqueue/apps.py
+++ mqueue/apps.py
@@ -18,9 +18,16 @@ class MqueueConfig(AppConfig):
             modsplit = modpath.split('.')
             path = '.'.join(modsplit[:-1])
             modname = '.'.join(modsplit[-1:])
-            module = importlib.import_module(path)
-            model = getattr(module, modname)
-            mqueue_tracker.register(model, level)
+            try:
+                module = importlib.import_module(path)
+                model = getattr(module, modname)
+                mqueue_tracker.register(model, level)
+            except ImportError:
+                from mqueue.models import MEvent
+                msg = "ERROR from Django Mqueue : can not import model " + modpath
+                MEvent.objects.create(
+                    name=msg, event_class="Error")
+
         # watchers
         from mqueue.watchers import init_watchers
         from mqueue.conf import WATCH
