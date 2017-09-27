cbe3900a502e7c158b93dadd5e5fa0c3ec50c746
Win <synwe@yahoo.com>
2017-09-27 10:07:06 +0200

    Fix bug in update signal + remove django-extensions dependency

--- mqueue/signals.py
+++ mqueue/signals.py
@@ -4,7 +4,7 @@ from mqueue.utils import get_user, get_url, get_admin_url, get_object_name
 
 
 def mmessage_create(sender, instance, created, **kwargs):
-    if created:
+    if created is True:
         # try to get the user
         user = get_user(instance)
         # try to get the object name
@@ -43,27 +43,28 @@ def mmessage_delete(sender, instance, **kwargs):
 
 
 def mmessage_save(sender, instance, created, **kwargs):
-    # try to get the user
-    user = get_user(instance)
-    if 'name' not in kwargs.keys():
-        # try to get the object name
-        obj_name = get_object_name(instance, user)
-    else:
-        obj_name = kwargs('name')
-    # try to get the admin url
-    admin_url = get_admin_url(instance)
-    event_str = ' edited'
-    if created:
-        event_str = ' created'
-    event_class = instance.__class__.__name__ + event_str
-    # create event
-    MEvent.objects.create(
-        model=instance.__class__,
-        name=obj_name,
-        obj_pk=instance.pk,
-        user=user,
-        url=get_url(instance),
-        admin_url=admin_url,
-        event_class=event_class,
-    )
+    if created is False:
+        # try to get the user
+        user = get_user(instance)
+        if 'name' not in kwargs.keys():
+            # try to get the object name
+            obj_name = get_object_name(instance, user)
+        else:
+            obj_name = kwargs('name')
+        # try to get the admin url
+        admin_url = get_admin_url(instance)
+        event_str = ' edited'
+        if created:
+            event_str = ' created'
+        event_class = instance.__class__.__name__ + event_str
+        # create event
+        MEvent.objects.create(
+            model=instance.__class__,
+            name=obj_name,
+            obj_pk=instance.pk,
+            user=user,
+            url=get_url(instance),
+            admin_url=admin_url,
+            event_class=event_class,
+        )
     return
--- mqueue/views.py
+++ mqueue/views.py
@@ -1,1 +1,7 @@
 # -*- coding: utf-8 -*-
+
+from django.views.generic import TemplateView
+
+
+class EventsDash(TemplateView):
+    template_name = "mqueue/dashboard.html"
