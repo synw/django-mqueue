73a4b8d8a6867ea69f6dbfdb247f0d7651133ea9
Win <synwe@yahoo.com>
2018-06-10 16:25:05 +0000

    Test managers

--- mqueue/tests/__init__.py
+++ mqueue/tests/__init__.py
@@ -221,7 +221,7 @@ class MqueueTest(TestCase):
         self.assertEqual(event_class_formated, html)
 
     """
-    Test hooks
+    Hooks
     """
 
     def test_redis_serializer(self):
@@ -256,23 +256,44 @@ class MqueueTest(TestCase):
         data = Pack(mevent)
         self.assertEqual(data, res)
 
+    """
+    Managers
+    """
 
-"""
-def test_managers(self):
-    user = User.objects.create_user('myuser', 'myemail@test.com', 'super_password')
-    mevent1 = MEvent.objects.create(name="event1", model=User)
-    mevent2 = MEvent.objects.create(name="event2", instance=user)
-    self.assertEqual(list(MEvent.objects.events_for_model(User)), [mevent2, mevent1])
-    self.assertEqual(MEvent.objects.count_for_model(User), 2)
-    self.assertEqual(list(MEvent.objects.events_for_object(user)), [mevent2])
-    return
+    def test_managers(self):
+        user = User.objects.create_user(
+            'myuser', 'myemail@test.com', 'super_password')
+        mevent1 = MEvent.objects.create(name="event1", model=User)
+        mevent2 = MEvent.objects.create(name="event2", instance=user)
+        self.assertEqual(
+            list(
+                MEvent.objects.events_for_model(User)), [
+                mevent2, mevent1])
+        self.assertEqual(MEvent.objects.count_for_model(User), 2)
+        self.assertEqual(
+            list(
+                MEvent.objects.events_for_object(user)),
+            [mevent2])
+        return
 
-def test_managers_with_event_class(self):
-    user = User.objects.create_user('myuser', 'myemail@test.com', 'super_password')
-    mevent1 = MEvent.objects.create(name="event1", model=User, event_class="class1")
-    mevent2 = MEvent.objects.create(name="event2", instance=user, event_class="class2")
-    self.assertEqual(list(MEvent.objects.events_for_model(User, event_classes=["class1"] )), [mevent1])
-    self.assertEqual(MEvent.objects.count_for_model(User, event_classes=["class2"]), 1)
-    self.assertEqual(list(MEvent.objects.events_for_object(user)), [mevent2])
-    return
-"""
+    def test_managers_with_event_class(self):
+        user = User.objects.create_user(
+            'myuser', 'myemail@test.com', 'super_password')
+        mevent1 = MEvent.objects.create(
+            name="event1", model=User, event_class="class1")
+        mevent2 = MEvent.objects.create(
+            name="event2", instance=user, event_class="class2")
+        self.assertEqual(
+            list(
+                MEvent.objects.events_for_model(
+                    User,
+                    event_classes=["class1"])),
+            [mevent1])
+        self.assertEqual(
+            MEvent.objects.count_for_model(
+                User, event_classes=["class2"]), 1)
+        self.assertEqual(
+            list(
+                MEvent.objects.events_for_object(user)),
+            [mevent2])
+        return
