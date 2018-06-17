52a79aae09a4d87054dbe5ced01e57d235d42a68
Win <synwe@yahoo.com>
2018-06-17 16:34:13 +0000

    Reorganize test folder

--- mqueue/tests/__init__.py
+++ mqueue/tests/__init__.py
@@ -1,22 +1,4 @@
-from .admin import MqueueTestAdmin
-from .create import MqueueTestCreate
-from .managers import MqueueTestManagers
-from .apps import MqueueTestApps
-from .utils import MqueueTestUtils
-from .hooks.redis import MqueueTestRedisHook
-from .watchers import MqueueTestWatchers
-from .signals import MqueueTestSignals
-#from .graphql_api import MqueueTestGraphqlApi
 
 
-class MqueueTest(MqueueTestAdmin,
-                 MqueueTestCreate,
-                 MqueueTestManagers,
-                 MqueueTestApps,
-                 MqueueTestUtils,
-                 MqueueTestWatchers,
-                 # MqueueTestGraphqlApi
-                 ):
-
-    def test_mqueue_logging(self):
-        from mqueue.logging import DEV_LOGGING, LOGGING_WARNING
+class MqueueTest():
+    pass
