23524c61e796dce59d37b71b64584b8ed67b3765
synw <synwx@protonmail.com>
2021-08-31 11:27:14 +0200

    Add long description to setup

--- setup.py
+++ setup.py
@@ -1,25 +1,32 @@
+from os import path
 from setuptools import setup, find_packages
 
 
-version = __import__('mqueue').__version__
+version = __import__("mqueue").__version__
+
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, "README.md"), encoding="utf-8") as f:
+    long_description = f.read()
 
 setup(
-    name='django-mqueue',
+    name="django-mqueue",
     packages=find_packages(),
     include_package_data=True,
     version=version,
-    description='Events queue for Django',
-    author='synw',
-    author_email='synwe@yahoo.com',
-    url='https://github.com/synw/django-mqueue',
-    download_url='https://github.com/synw/django-mqueue/releases/tag/' + version,
-    keywords=['django', 'logging', 'monitoring'],
+    description="Events queue for Django",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="synw",
+    author_email="synwe@yahoo.com",
+    url="https://github.com/synw/django-mqueue",
+    download_url="https://github.com/synw/django-mqueue/releases/tag/" + version,
+    keywords=["django", "logging", "monitoring"],
     classifiers=[
-        'Development Status :: 4 - Beta',
-        'Framework :: Django :: 1.11',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 3.8',
+        "Development Status :: 4 - Beta",
+        "Framework :: Django :: 1.11",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.8",
     ],
-    zip_safe=False
+    zip_safe=False,
 )
