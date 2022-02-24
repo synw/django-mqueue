f257c772873daf6002394ca1a080968b3e6da3d9
synw <synwx@protonmail.com>
2022-02-24 13:58:02 +0100

    Add livefeed templates and urls

---
+++ mqueue/urls.py
@@ -0,0 +1,9 @@
+from django.views.generic import TemplateView
+from django.urls import path
+
+urlpatterns = [
+    path(
+        "",
+        TemplateView.as_view(template_name="mqueue/livefeed/index.html"),
+    ),
+]
